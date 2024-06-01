5a707489c29f47e9f437d83177f3ff9ad42cb1b9
solipsismes <>
2024-06-01 22:42:54 +0200

    remove the python lib

--- tweet/README.md
+++ tweet/README.md
@@ -3,25 +3,7 @@
 * Go to [Twitter Developer Portal](https://developer.twitter.com). Set app
 permissions to read and write in settings>user authentication settings.
 
-* get API key and secret, bearer token, access token and secret in the "keys and
-  token" tab.
-
-
-* Install the required packages:
-  ```
-  python3 -m venv ~/.venv
-  source ~/.venv/bin/activate
-  python3 -m pip install tweepy python-dotenv
-  ```
-
-* Create a `.env` file with your keys and tokens:
-
-  ```
-  TWITTER_API_KEY="XXX"
-  TWITTER_API_KEY_SECRET="XXX"
-  TWITTER_ACCESS_TOKEN="XXX"
-  TWITTER_ACCESS_TOKEN_SECRET="XXX"
-  TWITTER_BEARER_TOKEN="XXX"
-  ```
+* get API key and secret, access token and secret in the "keys and
+  token" tab, copy them in the corresponding variables in `tweet.sh`
 
 * Done. You can now `./tweet.sh "Hello, world!"`
--- tweet/tweet.sh
+++ tweet/tweet.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
-set -e
-source ~/.venv/bin/activate
-python -u tweet.py $1
+
+urlencode () {
+        [ -n "$1" ] \
+        && echo -n "$1" | perl -p -e 's/([^A-Za-z0-9-._~])/sprintf("%%%02X", ord($1))/seg'
+}
+
+#oauth1 vars
+consumer_key=""
+consumer_secret=""
+token=""
+token_secret=""
+signature_method='HMAC-SHA1'
+timestamp=$(date +%s)
+nonce=$(md5 <<< "$RANDOM-$(date +%s.%N)" | cut -d' ' -f 1) # md5sum on linux
+
+url="https://api.twitter.com/2/tweets"
+
+encodedURL=$(urlencode $url)
+PARAM=$(urlencode "oauth_consumer_key=$consumer_key&oauth_nonce=$nonce&oauth_signature_method=$signature_method&oauth_timestamp=$timestamp&oauth_token=$token&oauth_version=1.0")
+signature=$(echo -n 'POST&'$encodedURL'&'$PARAM|openssl dgst -sha1 -binary -hmac "$consumer_secret&$token_secret" |base64)
+encoded_signature=$(urlencode $signature)
+
+curl -v   \
+  $url \
+  -H "Authorization: OAuth oauth_consumer_key=\"$consumer_key\",oauth_token=\"$token\",oauth_signature_method=\"$signature_method\",oauth_timestamp=\"$timestamp\",oauth_nonce=\"$nonce\",oauth_version=\"1.0\",oauth_signature=\"$encoded_signature\"" \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d "{ \"text\" : \"$1\"}"
